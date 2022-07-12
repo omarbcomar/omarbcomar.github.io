b3acb520663dfae1e12d6d6a068cbf6cd60921b9
omarbcomar <106739767+omarbcomar@users.noreply.github.com>
2022-07-12 17:10:30 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const textservicevice = document.getElementById("textservice");
+const carredegauche = document.querySelectorAll('.service-item')
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+carredegauche.forEach((item) => {
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 430) {
+		item.classList.add('slide-in-blurred-left');
+	} else;
+});
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
